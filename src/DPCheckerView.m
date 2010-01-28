c16e0883991ee027af6493bca44f21ec8c5d9ee4
Rasmus Andersson <rasmus@notion.se>
2010-01-28 18:18:34 +0100

    removed unused code

--- src/DPCheckerView.m
+++ src/DPCheckerView.m
@@ -3,12 +3,8 @@
 @implementation DPCheckerView
 
 - (void)drawRect:(NSRect)dirtyRect {
-	NSColor *color1, *color2;
-	NSRect r = [self bounds];
-	color1 = [NSColor colorWithPatternImage:[NSImage imageNamed:@"checker"]];
-
+	NSColor *color1 = [NSColor colorWithPatternImage:[NSImage imageNamed:@"checker"]];
 	[color1 set];
-
 	NSRectFill(dirtyRect);
 }
 
